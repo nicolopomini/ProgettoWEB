e81af8fa5663b4162827a5399282c101a23db24b
Gabriele Maurina <gabrielemaurina95@gmail.com>
2017-07-20 12:39:17 +0200

    Daos

---
+++ Progetto/src/main/java/dao/ComplaintDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import dao.entities.Complaint;
+import persistence.utils.dao.DAO;
+
+/**
+ *
+ * @author Gabriele
+ */
+public interface ComplaintDAO extends DAO<Complaint, Integer>{
+}
---
+++ Progetto/src/main/java/dao/PictureDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import dao.entities.Picture;
+import persistence.utils.dao.DAO;
+
+/**
+ *
+ * @author Gabriele
+ */
+public interface PictureDAO extends DAO<Picture, Integer>{
+}
