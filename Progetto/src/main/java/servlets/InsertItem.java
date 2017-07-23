be08ed6318c28d9c21a948d691a2ba9cd5521304
Gabriele Maurina <gabrielemaurina95@gmail.com>
2017-07-23 15:00:02 +0200

    set plural name

--- Progetto/src/main/java/servlets/InsertItem.java
+++ Progetto/src/main/java/servlets/InsertItem.java
@@ -5,7 +5,7 @@
  */
 package servlets;
 
-import Utility.MultipartHandler;
+import utils.MultipartHandler;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
--- Progetto/src/main/java/servlets/ModifyShop.java
+++ Progetto/src/main/java/servlets/ModifyShop.java
@@ -5,7 +5,7 @@
  */
 package servlets;
 
-import Utility.MultipartHandler;
+import utils.MultipartHandler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
