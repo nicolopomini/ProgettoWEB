6ce7a848c4486cb2e294c8b8687a2aa941906b0d
pomo <>
2017-11-09 18:17:33 +0100

    removed useless files

--- Progetto/scriptReview.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-fout = open("shporev","w")
-commenti = ["Ottimo negozio", "Ottimi prodotti, poca cortesia","Nella media","Poca assistenza al cliente","Poca gentilezza e disponibilita","Pessimo"]
-for i in range(500):
-	index = randint(0,4)
-	fout.write("('" + commenti[index] + "',null," + str(randint(1,100)) + "," + str(randint(1,200)) + ",now()," + str(index) + "),\n")
